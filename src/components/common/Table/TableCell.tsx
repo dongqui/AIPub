40af11ae600edea8532ad1d19212d9f35e488827
Dongjin <dongqui9@gmail.com>
2025-02-24 19:31:14 +0900

    refactor: table cell text align

--- src/components/common/Table/TableCell.tsx
+++ src/components/common/Table/TableCell.tsx
@@ -2,13 +2,18 @@ import { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {
   border?: boolean;
+  className?: string;
 }
-export default function TableCell({ children, border = true }: Props) {
+export default function TableCell({
+  children,
+  border = true,
+  className = "",
+}: Props) {
   return (
     <td
-      className={`p-4 vertical-align-center ${
+      className={`p-4 vertical-align-center text-center ${
         border ? "border-x border-gray-200" : ""
-      }`}
+      } ${className}`}
     >
       {children}
     </td>
